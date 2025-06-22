36ef3aca21d2dd4eda90a2ff8cf2db251db81bad
Crowley55555 <lavrovartem0511@gmail.com>
2025-06-22 15:57:12 +0300

    home-work is done

---
+++ bot/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class TelegramUser(models.Model):
+    user_id = models.BigIntegerField(unique=True)
+    username = models.CharField(max_length=255, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.user_id})"
---
+++ bot_main.py
@@ -0,0 +1,41 @@
+from telebot.types import Message
+import telebot
+import requests
+
+API_URL = 'http://127.0.0.1:8000/api/'
+BOT_TOKEN = '8159160029:AAFqOA0ec8ZlCkZgYBUIjbZ_jA2K007AqdA'
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start_command(message):
+    data = {
+        "user_id": message.from_user.id,
+        "username": message.from_user.username
+    }
+    response = requests.post(API_URL + "register/", json=data)
+    resp_json = response.json()
+    if 'message' in resp_json:
+        bot.send_message(message.chat.id, "Пользователь уже зарегистрирован")
+    elif 'id' in resp_json or 'user_id' in resp_json:
+        # В зависимости от того, какой ключ возвращает ваш сериализатор
+        bot.send_message(message.chat.id, f"Вы успешно зарегистрированы! Ваш user_id: {resp_json.get('user_id', resp_json.get('id'))}")
+    else:
+        bot.send_message(message.chat.id, "Произошла ошибка при регистрации!")
+        print(resp_json)
+        print(response.status_code)
+        print(response.text)
+
+@bot.message_handler(commands=['myinfo'])
+def user_info(message: Message):
+    response = requests.get(f"{API_URL}user/{message.from_user.id}/")
+    if response.status_code == 200:
+        bot.reply_to(message, f"Информация о пользователе:\n{response.text}")
+    elif response.status_code == 404:
+        bot.reply_to(message, "Пользователь не зарегистрирован")
+    else:
+        bot.reply_to(message, "Произошла ошибка при получении информации о пользователе")
+
+if __name__ == '__main__':
+    print("Бот запущен и ожидает сообщений...")
+    bot.infinity_polling()
